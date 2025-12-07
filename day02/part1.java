2a88f4b4ff00c1b333ae76106a55964887cfb100
Floriane <floriane.roy19@gmail.com>
2025-12-07 10:50:36 +0100

    init structure day02

---
+++ day02/part1.java
@@ -0,0 +1,10 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.AbstractMap.SimpleEntry;
+public class part1 {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
