0c740c494c1d59093c4f5a6b2008b76eacdc4129
Floriane <floriane.roy19@gmail.com>
2025-12-09 21:36:18 +0100

    init repo

---
+++ day04/part2.java
@@ -0,0 +1,10 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class part2 {
+
+    public static void main(String[] args) throws Exception {
+    }
+}
